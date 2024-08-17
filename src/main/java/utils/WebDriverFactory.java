37f157b7d669519c11baf19c72333061e21c30e6
Oleksandr Klants <oleksandr.klants@miquido.com>
2024-08-17 21:35:23 +0200

    Initial commit - Setup Maven project with Selenium, TestNG, and Allure

---
+++ src/main/java/utils/WebDriverFactory.java
@@ -0,0 +1,13 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverFactory {
+
+    public static WebDriver createWebDriver(){
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+}
