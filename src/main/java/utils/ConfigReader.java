e0cfadd33bf77ff147051e1f0cae01bbeba12664
Oleksandr Klants <oleksandr.klants@miquido.com>
2024-08-18 20:50:35 +0200

    Update configurations

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -1,20 +1,22 @@
 package utils;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConfigReader {
     private final Properties properties;
 
     public ConfigReader() throws FileNotFoundException {
+        final Logger LOGGER = Logger.getLogger(ConfigReader.class.getName());
         properties = new Properties();
        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")){
         properties.load(input);
        } catch (IOException e){
-        e.printStackTrace();
+           LOGGER.log(Level.SEVERE, "Failed to load configuration file.", e);
            throw new RuntimeException("Failed to load configuration file.");
        }
     }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,7 +3,7 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
+
 import utils.ConfigReader;
 import utils.WebDriverFactory;
 
