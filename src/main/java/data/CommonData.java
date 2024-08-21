3670152d414d8194c53d8b8eac42f34d78e97ae9
Oleksandr Klants <oleksandr.klants@miquido.com>
2024-08-21 10:47:35 +0200

    Add data files to store tet data, update config files

---
+++ src/main/java/data/CommonData.java
@@ -0,0 +1,8 @@
+package data;
+
+public class CommonData {
+    private CommonData() {}
+
+    public static final String ERROR_LOGIN_MESSAGE = "Authentication failed.";
+    public static final String SIGN_OUT_BTN_TEXT = "\uE7FF Sign out";
+}
---
+++ src/main/java/data/LoginData.java
@@ -0,0 +1,8 @@
+package data;
+
+public class LoginData {
+    private LoginData() {}
+
+    public static final String INVALID_EMAIL = "test1234@mail.com";
+    public static final String INVALID_PASSWORD = "test123";
+}
